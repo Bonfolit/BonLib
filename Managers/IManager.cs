33bad22fa520dffda185974ab8678ee84c5096d6
Bonfolit <onur.bulbul@outlook.com.tr>
2023-09-19 12:33:12 +0300

    Initial commit

---
+++ Managers/IManager.cs
@@ -0,0 +1,13 @@
+﻿namespace Gameplay.Managers
+{
+
+    public interface IManager
+    {
+        void BindDependencies();
+        void ResolveDependencies();
+        void SubscribeToEvents();
+        void Initialize();
+        void LateInitialize();
+    }
+
+}
